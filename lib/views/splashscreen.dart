89a2b0e7e0d7544e6022cc75db4186466e6eac7f
Asbiq Al Alawi <asbiqalalawi@gmail.com>
2021-03-19 09:40:25 +0700

    Icon

---
+++ lib/views/splashscreen.dart
@@ -0,0 +1,40 @@
+// import 'dart:async';
+// import 'package:flutter/material.dart';
+
+// import 'login_page.dart';
+
+// class SplashScreen extends StatefulWidget {
+//   @override
+//   _SplashScreenState createState() => _SplashScreenState();
+// }
+
+// class _SplashScreenState extends State<SplashScreen> {
+//   /* void initState() {
+//     super.initState();
+//     startSplashScreen();
+//   }
+
+//   startSplashScreen() async {
+//     var duration = const Duration(seconds: 3);
+//     return Timer(duration, () {
+//       Navigator.push(
+//         context,
+//         MaterialPageRoute(builder: (context) => LoginPage()),
+//       );
+//     });
+//   } */
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       body: Container(
+//         height: 300,
+//         width: 300,
+//         child: Image(
+//           image: AssetImage('assets/launcher/bapenda.png'),
+//         ),
+//       ),
+//     );
+//   }
+// }
